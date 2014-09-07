5497540582de6dee6fafd75b24a3ecfbdc99c143
Oscar Berry <oscar@madokami.com>
2014-09-07 03:08:09 +0100

    banners

---
+++ banners.php
@@ -0,0 +1,13 @@
+<?php
+
+$banners = array(
+	IMAGETYPE_PNG
+);
+
+$banner = array_rand($banners);
+$type = $banners[$banner];
+
+header('Content-Type: '.image_type_to_mime_type($type));
+readfile('banners/'.$banner.image_type_to_extension($type, true));
+
+?>
