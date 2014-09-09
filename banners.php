5d4f3fb8c9621b1c8e1a64c67bfdf3d64b0bf5c6
Oscar Berry <oscar@madokami.com>
2014-09-09 19:48:13 +0100

    even more banners

--- banners.php
+++ banners.php
@@ -1,12 +1,17 @@
 <?php
 
 $banners = array(
-	IMAGETYPE_PNG,
-    IMAGETYPE_PNG,
-    IMAGETYPE_PNG,
-    IMAGETYPE_PNG,
-    IMAGETYPE_PNG,
-    IMAGETYPE_PNG
+	0 => IMAGETYPE_PNG,
+    1 => IMAGETYPE_PNG,
+    2 => IMAGETYPE_PNG,
+    3 => IMAGETYPE_PNG,
+    4 => IMAGETYPE_PNG,
+    5 => IMAGETYPE_PNG,
+    6 => IMAGETYPE_GIF,
+    7 => IMAGETYPE_PNG,
+    8 => IMAGETYPE_GIF,
+    9 => IMAGETYPE_PNG,
+    10 => IMAGETYPE_JPEG
 );
 
 $banner = array_rand($banners);
